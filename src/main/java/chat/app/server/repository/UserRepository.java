153ed3a7d6a96df038c091a8f7470a16b5067cdd
Fernando Romero <fer.rom.mu@gmail.com>
2023-02-06 11:48:50 -0300

    Fix auth.

--- src/main/java/chat/app/server/repository/UserRepository.java
+++ src/main/java/chat/app/server/repository/UserRepository.java
@@ -6,5 +6,6 @@ import chat.app.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 
 }
